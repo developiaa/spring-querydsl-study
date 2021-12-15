6db438ef2e7cdb42fae36d6fcc1dc6808d1137c7
developiaa <developiaa@gmail.com>
2021-12-15 23:45:00 +0900

    test : Add join on

--- src/test/java/study/developia/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/developia/querydsl/QuerydslBasicTest.java
@@ -204,7 +204,7 @@ public class QuerydslBasicTest {
      * 팀의 이름과 각 팀의 평균 연력을 구해라
      */
     @Test
-    void group(){
+    void group() {
         List<Tuple> result = queryFactory
                 .select(team.name, member.age.avg())
                 .from(member)
@@ -228,7 +228,7 @@ public class QuerydslBasicTest {
      * 팀 A에 소속된 모든 회원
      */
     @Test
-    void join(){
+    void join() {
         List<Member> result = queryFactory.selectFrom(member)
                 .join(member.team, team)
                 .where(team.name.eq("teamA"))
@@ -250,7 +250,7 @@ public class QuerydslBasicTest {
      * 회원의 이름이 팀 이름과 같은 회원 조회
      */
     @Test
-    void theta_join(){
+    void theta_join() {
         em.persist(new Member("teamA"));
         em.persist(new Member("teamB"));
         em.persist(new Member("teamC"));
@@ -267,6 +267,45 @@ public class QuerydslBasicTest {
     }
 
 
+    /**
+     * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL : select m, t from Member m left join m.team t on t.name='teamA'
+     */
+    @Test
+    void join_on_filtering() {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team)
+                .on(team.name.eq("teamA"))
+                .fetch();
 
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+
+    /**
+     * 연관관계가 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 대상 외부 조인
+     */
+    @Test
+    void join_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team)
+                .on(member.username.eq(team.name))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
 
 }
