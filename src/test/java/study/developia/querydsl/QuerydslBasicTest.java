f209e5acbd7a17b5e9230f45f81237cd8dc48f20
developiaa <developiaa@gmail.com>
2022-02-27 23:26:46 +0900

    feat : Add contant, string value

--- src/test/java/study/developia/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/developia/querydsl/QuerydslBasicTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.deser.std.StdKeyDeserializer;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
@@ -447,6 +448,33 @@ public class QuerydslBasicTest {
                 .fetch();
 
         for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    //상수
+    @Test
+    void constant() {
+        List<Tuple> result = queryFactory.select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    //문자 더하기
+    //stringValue()를 통해서 문자 처리, enum값 처리할 때도 사용
+    @Test
+    void concat() {
+        //{username}_{age}
+        List<String> fetch = queryFactory.select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetch();
+
+        for (String s : fetch) {
             System.out.println("s = " + s);
         }
     }
