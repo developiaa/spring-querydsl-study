819aee5863cbc2f967f9f5b2860ad85412acdce8
developiaa <developiaa@gmail.com>
2022-03-07 22:37:46 +0900

    feat : Add paging logic

--- src/main/java/study/developia/querydsl/repository/MemberRepositoryCustom.java
+++ src/main/java/study/developia/querydsl/repository/MemberRepositoryCustom.java
@@ -1,5 +1,7 @@
 package study.developia.querydsl.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import study.developia.querydsl.dto.MemberSearchCondition;
 import study.developia.querydsl.dto.MemberTeamDto;
 
@@ -7,4 +9,6 @@ import java.util.List;
 
 public interface MemberRepositoryCustom {
     List<MemberTeamDto> search(MemberSearchCondition condition);
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+    Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
 }
